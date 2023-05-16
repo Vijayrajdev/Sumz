b3646bdbdef277b323da72914a880f7ffc0b2db3
Vijayrajdev <vijayrajck@outlook.com>
2023-05-16 19:06:39 +0530

    Initial commit

---
+++ src/components/Demo.jsx
@@ -0,0 +1,5 @@
+const Demo = () => {
+  return <div>Demo</div>;
+};
+
+export default Demo;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import { logo } from "../assets";
+const Hero = () => {
+  return (
+    <div>
+      <header className="w-full flex justify-center items-center flex-col"></header>
+    </div>
+  );
+};
+
+export default Hero;
