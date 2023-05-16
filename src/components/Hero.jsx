ebae5ade047cb2de3f739a0ad1cf935c8e6e80fb
Vijayrajdev <vijayrajck@outlook.com>
2023-05-17 00:30:48 +0530

    Build completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,27 @@
 import { logo } from "../assets";
 const Hero = () => {
   return (
-    <div>
-      <header className="w-full flex justify-center items-center flex-col"></header>
-    </div>
+    <header className=" w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img className="w-28 object-contain" src={logo} alt="sumz-logo" />
+        <button
+          className="black_btn"
+          type="button"
+          onClick={() => window.open("https://github.com/Vijayrajdev/Sumz")}
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
   );
 };
 
